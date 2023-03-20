e346d94b4f2ba1ac704ca8f9935bb9d35353ccfe
Jignesh Makwana <makwana.jignesh@gmail.com>
2023-03-21 00:23:38 +0800

    Update code to run api

--- scripts/startapp.sh
+++ scripts/startapp.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#Install compose
+composer install
+
+#Install npn
+npm install
+npm run build
+composer require livewire/livewire
+
+php artisan livewire:publish --config
+php artisan livewire:publish --assets
+
+#Php command
+php artisan key:generate
+php artisan optimize:clear
+php artisan serve
